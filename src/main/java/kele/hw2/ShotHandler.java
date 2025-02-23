d8a535d7a2a22ad53050b76401e9788c070550ad
Smaghead <kelemen.da@gmail.com>
2025-02-23 20:17:05 +0100

    Extended ShotHandler to handle ship HPs and win condition

--- src/main/java/kele/hw2/ShotHandler.java
+++ src/main/java/kele/hw2/ShotHandler.java
@@ -4,9 +4,16 @@ import java.util.HashMap;
 
 
 public class ShotHandler {
-    public HashMap<String, String[]> dataToShow = new HashMap<>();
-    public HashMap<String, String[]> shipData = new HashMap<>();
-    public static final String[] rowNames = {"A", "B", "C", "D", "E", "F"};
+    private Boolean isGameRunning = true;
+    protected HashMap<String, String[]> dataToShow = new HashMap<>();
+    protected HashMap<String, String[]> shipData = new HashMap<>();
+    protected static final String[] rowNames = {"A", "B", "C", "D", "E", "F"};
+    private Integer carrierHp = 5;
+    private Integer battleshipHp = 4;
+    private Integer cruiserHp = 3;
+    private Integer submarineHp = 2;
+    private Integer destroyerHp = 1;
+
 
     public void fireAction(Board player) {
         Boolean isRepeatNecessary = true;
@@ -24,8 +31,11 @@ public class ShotHandler {
     private void fireCommand(String row, Integer column, Board enemy) throws InvalidMoveException {
         if (Utility.isShipThere(row, column, enemy) && Utility.isValidMove(row, column, (Board) this)) {
             System.out.println("Ship hit on cell: " + row + column);
+            String shipSign = getShipSign(row, column, enemy);
+            deductShipHp(shipSign);
             addHit(row, column, enemy, (Board) this);
-            //TODO deduct point from ship life
+            printDestroyedShip();
+            setIsGameRunning(enemy);
         }else if (Utility.isValidMove(row, column, (Board) this)) {
             System.out.println("Ship missed on cell: " + row + column);
             addMiss(row, column, (Board) this);
@@ -46,9 +56,72 @@ public class ShotHandler {
         technicalShip[column - 1] = "x";
     }
 
-    //TODO add method to get ship sign
     private String getShipSign(String row, Integer column, Board player) {
         String[] technical = player.shipData.get(row);
         return technical[column - 1];
+    }
+
+    private void deductShipHp(String string) {
+        switch (string) {
+            case "1" -> destroyerHp--;
+            case "2" -> submarineHp--;
+            case "3" -> cruiserHp--;
+            case "4" -> battleshipHp--;
+            case "5" -> carrierHp--;
+        }
+    }
+
+    private void printDestroyedShip(){
+        if (destroyerHp == 0) {
+            System.out.println("The enemy's Destroyer has been sunk! (length 1)");
+            destroyerHp = 69;
+        }
+        if (submarineHp == 0) {
+            System.out.println("The enemy's Submarine has been sunk! (length 2)");
+            submarineHp = 69;
+        }
+        if (cruiserHp == 0) {
+            System.out.println("The enemy's Cruiser has been sunk! (length 3)");
+            cruiserHp = 69;
+        }
+        if (battleshipHp == 0) {
+            System.out.println("The enemy's Battleship has been sunk! (length 4)");
+            battleshipHp = 69;
+        }
+        if (carrierHp == 0) {
+            System.out.println("The enemy's Carrier has been sunk! (length 5)");
+            carrierHp = 69;
+        }
+    }
+
+    private void setIsGameRunning(Board enemy) {
+        if (enemy.getDestroyerHp() == 69
+                && enemy.getSubmarineHp() == 69
+        && enemy.getCruiserHp() == 69
+        && enemy.getBattleshipHp() ==69
+        && enemy.getCarrierHp() == 69) {
+            isGameRunning = false;
+        }
+        isGameRunning = true;
+    }
+
+    public Integer getCarrierHp() {
+        return carrierHp;
+    }
+
+    public Integer getBattleshipHp() {
+        return battleshipHp;
+    }
+
+    public Integer getCruiserHp() {
+        return cruiserHp;
+    }
+
+    public Integer getSubmarineHp() {
+        return submarineHp;
+    }
+
+    public Integer getDestroyerHp() {
+        return destroyerHp;
     }
 }
